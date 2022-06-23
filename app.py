7f05a9933d2537e58f89b96ffa69e9338d9c4016
Anand Mahesh <manand881@gmail.com>
2022-06-23 10:35:48 +0530

    fastapi

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
+from distutils.log import debug
 from fastapi import FastAPI
+import uvicorn
+import subprocess
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World anand"}
+
+
+if "__main__" == __name__:
+    uvicorn.run("app:app", debug=True, reload=True)
